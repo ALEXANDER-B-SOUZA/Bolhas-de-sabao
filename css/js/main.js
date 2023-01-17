26b0cf65d65b8d6e150b9643f6515d2c9255ab7c
Alexander-Souza <bsalex12010@gmail.com>
2023-01-17 20:26:52 -0300

    Colocando imagem de fundo.

--- css/js/main.js
+++ css/js/main.js
@@ -1,4 +1,19 @@
+/*Criando variáveis*/
+let posicaox;
+let posicaoy;
+let velocidade;
+
 /*Criando a tela da aplicação.*/
 function setup(){
-createCanvas(400, 400)
+createCanvas(400, 400);
+posicaox = whidh/2;
+posicaoy = heigth/2;
+velocidade = 2;
+}
+while()
+function draw(){
+background(55,20,200);
+circle(posicaox, posicaoy, 30);
+posicaox = posicaox + velocidade;
+posicaoy = posicaoy +velocidade;
 }
